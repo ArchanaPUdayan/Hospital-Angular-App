38ca764dd7aa6d899031ebdd7ef2758bf122db6f
Archanapudayan <atchanaudayan64534@gmail.com>
2022-11-26 17:16:09 +0530

    Hospitalsentry

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PatiententryComponent } from './patiententry/patiententry.component';
+import { HospitalsEntryComponent } from './hospitals-entry/hospitals-entry.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PatiententryComponent
+    PatiententryComponent,
+    HospitalsEntryComponent
   ],
   imports: [
     BrowserModule,
