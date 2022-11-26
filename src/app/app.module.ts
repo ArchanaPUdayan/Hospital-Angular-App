9d64cb39ee849420235a2cd59e6d2dd11c480398
Archanapudayan <atchanaudayan64534@gmail.com>
2022-11-26 20:32:39 +0530

    pateintdelete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { PatiententryComponent } from './patiententry/patiententry.component';
 import { HospitalsEntryComponent } from './hospitals-entry/hospitals-entry.component';
 import { PatientSearchComponent } from './patient-search/patient-search.component';
+import { PatientsearchComponent } from './patientsearch/patientsearch.component';
+import { PatientDeleteComponent } from './patient-delete/patient-delete.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PatiententryComponent,
     HospitalsEntryComponent,
-    PatientSearchComponent
+    PatientSearchComponent,
+    PatientsearchComponent,
+    PatientDeleteComponent
   ],
   imports: [
     BrowserModule,
